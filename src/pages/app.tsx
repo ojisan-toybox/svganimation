84f49648c1494909c1d715f8ef8f2933f21e9fdd
sadness_ojisan <sadness.ojisan@gmail.com>
2020-10-07 01:45:45 +0900

    chore transition

--- src/pages/app.tsx
+++ src/pages/app.tsx
@@ -1,9 +1,13 @@
 import { h } from "preact";
+import { useState } from "preact/compat";
 import { styled } from "goober";
 
 interface PassedProps {}
 
-interface ContainerProps {}
+interface ContainerProps {
+  state: boolean;
+  setState: (state: boolean) => void;
+}
 
 interface Props extends PassedProps, ContainerProps {
   className?: string;
@@ -11,7 +15,15 @@ interface Props extends PassedProps, ContainerProps {
 
 const Component = (props: Props) => (
   <div className={props.className}>
-    <svg className="menu" width="250px" height="250px" viewBox="0 0 500 500">
+    <svg
+      className="menu"
+      width="250px"
+      height="250px"
+      viewBox="0 0 500 500"
+      onClick={() => {
+        props.setState(!props.state);
+      }}
+    >
       <path
         className="border1 border"
         d="M44.6,151.6h409.9c18.9,0,34.2-15.2,34.2-34.1c0-18.8-15.3-34-34.2-34H44.6c-18.8,0-34.1,15.2-34.1,34 C10.4,136.4,25.7,151.6,44.6,151.6z"
@@ -28,34 +40,32 @@ const Component = (props: Props) => (
   </div>
 );
 
-const StyledComponent = styled(Component)`
+const StyledComponent = styled(Component)<ContainerProps>`
   & > .menu {
     .border {
-      fill: rgb(0, 0, 0);
+      fill: ${(props) => (props.state ? "#890a0c" : "rgb(0, 0, 0)")};
       transition: 200ms all ease;
       transform-origin: 250px 250px;
     }
-    &:hover {
-      .border {
-        fill: #890a0c;
-      }
-      /* 1本目のボーダーは、ホバー時に回転させる */
-      .border1 {
-        transform: translate(-102px, 102px) rotate(45deg);
-      }
-      /* 2本目のボーダーは、ホバー時に透明にする */
-      .border2 {
-        opacity: 0;
-      }
-      .border3 {
-        transform: translate(-102px, -102px) rotate(-45deg);
-      }
+    .border1 {
+      transform: ${(props) =>
+        props.state ? "translate(-102px, 102px) rotate(45deg)" : "initial"};
+    }
+    .border2 {
+      opacity: ${(props) => (props.state ? 0 : 1)};
+    }
+
+    .border3 {
+      transform: ${(props) =>
+        props.state ? "translate(-102px, -102px) rotate(-45deg);" : "initial"};
     }
   }
 `;
 
 const ContainerComponent = () => {
-  return <StyledComponent></StyledComponent>;
+  const [state, setState] = useState(false);
+  const containerProps = { state, setState };
+  return <StyledComponent {...containerProps}></StyledComponent>;
 };
 
 export const App = ContainerComponent;
